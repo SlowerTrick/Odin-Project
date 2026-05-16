e3f50716d7a4f4982c7282cd427f8b497aec4b41
SlowerTrick <arturvitorcintra@gmail.com>
2026-05-16 16:30:03 -0300

    vitest update

--- React/vitest/test1/tests/App.test.jsx
+++ React/vitest/test1/tests/App.test.jsx
@@ -3,7 +3,7 @@
 import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import App from "./App";
+import App from "../src/App.jsx";
 
 describe("App component", () => {
     it("renders magnificent monkeys", () => {
